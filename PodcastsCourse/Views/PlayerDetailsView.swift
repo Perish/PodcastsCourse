812f0220d0be9c006787757b962786432f24d701
徐冲 <xuchong@xuchongdemacbook-pro.local>
2018-08-20 20:25:31 +0800

    launching our Podcast Player

---
+++ PodcastsCourse/Views/PlayerDetailsView.swift
@@ -0,0 +1,29 @@
+//
+//  PlayerDetailsView.swift
+//  PodcastsCourse
+//
+//  Created by 徐冲 on 2018/8/20.
+//  Copyright © 2018年 徐冲. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDetailsView: UIView {
+    
+    var episode: Episode! {
+        didSet {
+            titleLabel.text = episode.title
+            
+            guard let url = URL(string: episode.imageUrl ?? "") else {return}
+            episodeImageView.sd_setImage(with: url, completed: nil)
+        }
+    }
+    
+    
+    @IBAction func handleDismiss(_ sender: Any) {
+        self.removeFromSuperview()
+    }
+    
+    @IBOutlet weak var episodeImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+}
