3149eca7b716d1426c3512dd94db6b0d55ce4421
徐冲 <xuchong@xuchongdemacbook-pro.local>
2018-08-21 21:01:22 +0800

    play episode with AVPlayer

--- PodcastsCourse/Views/PlayerDetailsView.swift
+++ PodcastsCourse/Views/PlayerDetailsView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit
 
 class PlayerDetailsView: UIView {
     
@@ -15,16 +16,51 @@ class PlayerDetailsView: UIView {
             titleLabel.text = episode.title
             authorLabel.text = episode.author
             
+            playEpisode()
+            
             guard let url = URL(string: episode.imageUrl ?? "") else {return}
             episodeImageView.sd_setImage(with: url, completed: nil)
         }
     }
     
+    fileprivate func playEpisode() {
+        print("try to play episode url: ", episode.streamUrl)
+        
+        guard let url = URL(string: episode.streamUrl) else {return}
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+        
+    }
+    
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
+    
+    //MARK:- IB Actions and Outlet
     
     @IBAction func handleDismiss(_ sender: Any) {
         self.removeFromSuperview()
     }
-    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton! {
+        didSet {
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            playPauseButton.addTarget(self, action: #selector(handlePlayPause), for: .touchUpInside)
+        }
+    }
+    
+    @objc func handlePlayPause() {
+        print("Trying to play and pause")
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+        } else {
+            player.pause()
+            playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+        }
+    }
     
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var episodeImageView: UIImageView!
